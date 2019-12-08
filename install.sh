9c4adb423bf5cd16ce9d78701ef9010b4b05f2e9
weymoz <weymoze@gmail.com>
2019-12-08 22:24:20 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,2 @@
+npm i -D webpack webpack-cli webpack-dev-server style-loader css-loader html-webpack-plugin sass-loader node-sass @babel/core @babel/preset-env @babel/preset-react babel-loader
+npm i react react-dom
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ]
+}
